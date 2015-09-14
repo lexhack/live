47a104c8a18a894e9d7cb84df7694ed0585f1d4d
Noah Moroze <noahm@moroze.com>
2015-09-14 11:46:33 -0400

    Twitter integration for live feed

--- feed.js
+++ feed.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
 	}
 
 	updateSkills(); 
+	updateAnnouncements(); 
 
 	$("#summon-mentor").on("click", function() {
 		$.ajax({
@@ -27,7 +28,40 @@ $(document).ready(function() {
 		    }
 		}); 
 	});
+
+	setInterval(updateAnnouncements, 30000); 
 }); 
+
+var updateAnnouncements = function() {
+	$.ajax({
+		url: "http://localhost:3000/announcements",
+		type:"GET",
+		dataType: "json",
+		success: function(json) {
+			$("#announcements-list").text(""); 
+			$.each(json, function(index, value) {
+				var d = new Date(value.date); 
+				$("#announcements-list")
+					.append($("<h4></h4")
+						.html(replaceURLWithHTMLLinks(value.body)))
+					.append($("<p></p>")
+						.text(d.toLocaleTimeString()));
+			})
+		},
+		error: function(xhr, status, error) {
+			console.log("ERROR");
+			console.log(status); 
+		},
+		complete: function(xhr, status) {
+
+		}
+	})
+};
+
+var replaceURLWithHTMLLinks = function(text) {
+    var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i;
+    return text.replace(exp,"<a href='$1'>$1</a>"); 
+}
 
 var updateSkills = function() {
 	$.ajax({
